e2f1020428e3eca68c986c3ac851b591511f139b
wilgustavo <wilgustavo@gmail.com>
2012-03-17 19:16:51 -0300

    javascript: Inner function

--- javascript/practicas/00014.js
+++ javascript/practicas/00014.js
@@ -1,11 +1,13 @@
 // Inner private function
 
-function a(num){
-    function b(input){
+var a = function(num){
+    var b = function(input){
         return input * 2;
     };
     return 'The result is ' + b(num);
 }
 
+
 console.assert('The result is 4' === a(2) , "inner function 1");
 console.assert('The result is 10' === a(5) , "inner function 2");
+console.assert( typeof b === 'undefined' , "b is local function");
